443d30f4694fa118118bb2dd1f693a627d46a626
n1ck <a_nick@inbox.ru>
2015-10-07 18:30:57 +0300

    Initial commit

---
+++ app/src/main/java/com/nutsdev/databindingtest/MovieClickHandler.java
@@ -0,0 +1,10 @@
+package com.nutsdev.databindingtest;
+
+import android.view.View;
+
+public interface MovieClickHandler{
+
+    void onNewClick(View view);
+    void onWatchedClick(View view);
+
+}
